922bad6c3c5ecb0e0ba4534a0ae7118cda46d816
Devshimitsu <65497645+Devshimitsu@users.noreply.github.com>
2021-03-26 21:30:05 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+  const btn = document.querySelector('.button');
+  const hexColor = document.querySelector('.hex-color');
+
+  let newColor;
+
+  const generate = () => {
+    newColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+
+    if(newColor.length < 7) {
+      generate();
+    }
+  }
+
+  btn.addEventListener('click', () => {
+    generate();
+
+    document.body.style.backgroundColor = newColor;
+    btn.style.color = newColor;
+    hexColor.value = newColor;
+  })
+
+
+// By Devshimitsu
